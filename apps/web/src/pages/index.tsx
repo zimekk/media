d385f44334f6f8ce7af20c7f32c9c2ce83d23654
zimekk <zimekk@gmail.com>
2023-04-28 23:48:15 +0200

    chore: fix deps and linter

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import type { NextPage } from "next";
 import Head from "next/head";
-// import { signIn, signOut } from "next-auth/react";
 
 import { Hello } from "@dev/hello";
-// import { Music } from "@dev/music";
 
 export default function Web() {
   return (
@@ -14,10 +10,10 @@ export default function Web() {
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main>
-      <h1>Web</h1>
-      <Hello/>
-    </main>
+      <main>
+        <h1>Web</h1>
+        <Hello />
+      </main>
     </>
   );
 }
